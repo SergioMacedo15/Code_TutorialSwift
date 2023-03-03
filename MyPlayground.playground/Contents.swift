7f4b59106f4493e82d7b3d0318d3a379015a06b8
Sergio Silva Macedo <sergio.m15@aluno.ifsc.edu.br>
2023-03-03 18:54:17 -0300

    My first day in the introduction Xcode Course
    The fast introduction about the data types in xcode

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -1,3 +1,56 @@
 import UIKit
 
-var greeting = "Hello, playground"
+
+
+// Primitive Types -> Int, String, Bool, Set,
+
+// PART 1 - Int
+
+//var variavel : [Int] = [1,2,3,4,6]
+// for declaret a empty variable or let (constant), you need use a begginer declaration ex: varivel : [tyoe]=[]
+
+//variavel.count
+//variavel.last
+//variavel.insert(99, at: 0)
+//variavel.shuffle()
+//print(variavel)
+
+// PART 2 -- Set
+
+//var ages = [18, 33, 44, 55, 12 , 18, 18]
+//// OBS: Set dont addmit a duplicate data
+//// For declaret a empty variable type Set , you need to use a begginer declaration ex: var agesSet: Set<Int> = []
+//var setAges : Set<Int>
+//
+//var agesSet = Set(ages)
+//agesSet.contains(18)
+//print(agesSet)
+
+//Part 3 -- Dictionary
+////// Dictionary is different of the Class, is  like a real dictionary with your key word and your value respective
+//var Cars : [String:String]=[
+//"Carro": "Ferrari",
+//"Laptop" : "IPad Pro 11"
+//]
+//// The empty dictionary is not expected, but the declaration is like var Cars : [DataType Key:DataType Value] = [key:Value,....]
+//
+//Cars["Laptop"]
+
+
+//Part 4 - Functions
+
+//// func "Name of the function" ( Name parameters : Data Type ) "obs: this next part is optional, is a pre declaration of the data type return of that function" -> Int {}
+//func SomaNumero(Value1: Float , Value2 : Float ) -> Float{
+//    var soma = Value1 + Value2
+//    return soma
+//}
+//
+//print(SomaNumero(Value1: 4.4, Value2: 1))
+
+//Part 5 - for loop
+
+let AllStars = ["Sergio", "Harden", "James","Test"]
+
+for Players in AllStars{
+    print(Playersl)
+}
